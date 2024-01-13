226cb420083cd3c9e41233a11ada3db544724354
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-01-13 17:29:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,6 +21,11 @@ rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
+# 克隆 coolsnowwolf/luci 仓库
+git clone https://github.com/coolsnowwolf/luci.git
+
+# 复制 luci-app-airplay2 文件夹到 feeds/luci/applications/
+cp -r luci/applications/luci-app-airplay2/ feeds/luci/applications/luci-app-airplay2/
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate

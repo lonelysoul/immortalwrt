2409c32986447cf0c31fbd0b4843b5dbed698f08
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-01-13 16:40:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,12 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
-# 克隆 coolsnowwolf/luci 仓库
-git clone https://github.com/coolsnowwolf/luci.git
-
-# 复制 luci-app-airplay2 文件夹到 feeds/luci/applications/
-cp -r luci/applications/luci-app-airplay2/ feeds/luci/applications/
-
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate

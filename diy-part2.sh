7b55365369a57118959d45dfda9bb75537f227c8
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-01-15 15:09:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,21 +11,21 @@
 #
 
 # 删除 golang 语言包
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 # 删除 feeds 中的 v2ray-geodata 包（适用于 openwrt-22.03 和 master）
-rm -rf feeds/packages/net/v2ray-geodata
+# rm -rf feeds/packages/net/v2ray-geodata
 
 # 克隆 mosdns 和 v2ray-geodata 仓库
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 克隆 coolsnowwolf/luci 仓库
-git clone https://github.com/coolsnowwolf/luci.git
+# git clone https://github.com/coolsnowwolf/luci.git
 
 # 复制 luci-app-airplay2 文件夹到 feeds/luci/applications/
-cp -r luci/applications/luci-app-airplay2/ feeds/luci/applications/luci-app-airplay2/
+# cp -r luci/applications/luci-app-airplay2/ feeds/luci/applications/luci-app-airplay2/
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate

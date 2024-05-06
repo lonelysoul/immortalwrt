dad05deb8ea4b6f09e72e7350c3164ca48b6ecca
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-05-06 18:28:03 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,14 +8,23 @@ git fetch origin
 LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse origin/master)
 
-# 如果本地代码不是最新的，则更新并重新编译
-if [ $LOCAL != $REMOTE ]; then
+# 检查本地和远程代码是否一致
+if [ "$LOCAL" != "$REMOTE" ]; then
     echo "Source code has been updated. Recompilation starts."
+    # 开始计时
+    start=$(date +%s)
+    # 拉取最新代码并重新编译
     git pull origin master && \
     ./scripts/feeds update -a && \
     ./scripts/feeds install -a && \
     make -j8 download && make -j3 && \
+    # 结束计时
+    end=$(date +%s)
+    # 输出编译总耗时
+    echo "Total compilation time: $((end-start)) seconds"
+    # 移除旧的记录文件，更新为最新的
     echo "$(date): Compilation completed"
 else
+    # 本地代码是最新的，无需重新编译
     echo "Source code is up to date. No need to recompile."
 fi

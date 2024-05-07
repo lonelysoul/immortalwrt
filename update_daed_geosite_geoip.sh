391b4255db2e67bcceb67a77860172f1451a1192
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-05-07 15:24:23 +0800

    Create update_daed_geosite_geoip.sh

---
+++ update_daed_geosite_geoip.sh
@@ -0,0 +1,2 @@
+wget -O /usr/share/daed/geoip.dat https://github.com/v2rayA/dist-v2ray-rules-dat/raw/master/geoip.dat
+wget -O /usr/share/daed/geosite.dat https://github.com/v2rayA/dist-v2ray-rules-dat/raw/master/geosite.dat

78c854f25fab03aa1900527e14cf9e297e101164
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-06-01 17:25:24 +0800

    Update init_install.sh

--- init_install.sh
+++ init_install.sh
@@ -6,20 +6,31 @@ sudo apt-get update
 # Installing dependencies
 sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev libpython3-dev libreadline-dev libssl-dev libtool lld llvm lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 
-# Cloning the ImmortalWrt source code
-git clone -b master --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt immortalwrt
+# Checking if the immortalwrt directory exists
+if [ ! -d "immortalwrt" ]; then
+    # Cloning the ImmortalWrt source code since the directory does not exist
+    git clone -b master --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt immortalwrt
+else
+    echo "The directory 'immortalwrt' already exists. Skipping clone operation."
+fi
+
+# Changing directory to immortalwrt
+cd immortalwrt || exit
 
 # Updating the feeds
-cd immortalwrt && ./scripts/feeds update -a
+./scripts/feeds update -a
 
 # Installing all feeds
 ./scripts/feeds install -a
 
-# Downloading the .config file
-wget https://raw.githubusercontent.com/lonelysoul/immortalwrt/main/.config
+# Overwriting the .config file and showing a prompt
+if [ -f ".config" ]; then
+    echo "The .config file already exists and will be overwritten."
+fi
+wget -O .config https://raw.githubusercontent.com/lonelysoul/immortalwrt/main/.config
 
 # Completing the configuration using defconfig
 make defconfig
 
-# back to main
+# Changing directory 
 cd

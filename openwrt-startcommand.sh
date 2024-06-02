650e07b2bfe420f044a2d0b2734925cd5a4b4cb2
lonelysoul <wangchengming@outlook.com>
2024-06-02 10:29:30 +0800

    修改openwrt的hysteria启动脚本

--- openwrt-startcommand.sh
+++ openwrt-startcommand.sh
@@ -1,2 +1,2 @@
 sleep 3 && cd /etc/sing-box/ && sing-box run  &>/dev/null &
-sleep 3 &&  cd /usr/bin && hysteria  &>/dev/null &
+sleep 10 && cd /root/ && hysteria -c /etc/config/hyster.config.yaml  &>/dev/null & 

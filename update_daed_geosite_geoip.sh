bb32c5566372ff7896f29744d8bd026e67fb2afa
lonelysoul <wangchengming@outlook.com>
2024-06-02 10:24:14 +0800

    删除/移动无用的JSON

--- update_daed_geosite_geoip.sh
+++
@@ -1,2 +0,0 @@
-wget -O /usr/share/daed/geoip.dat https://github.com/v2rayA/dist-v2ray-rules-dat/raw/master/geoip.dat
-wget -O /usr/share/daed/geosite.dat https://github.com/v2rayA/dist-v2ray-rules-dat/raw/master/geosite.dat

58ea5d3d67b30312b2f165de9f81e3a4d158fc58
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-09 22:58:03 +0800

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -17,16 +17,7 @@ mkdir -p /run/sshd
 echo 'root:root' | chpasswd
 
 # 安装开发和编译相关的工具
-apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison \
-build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj \
-fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf \
-haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev \
-libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev \
-libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs \
-msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip \
-python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons \
-squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto \
-xxd zlib1g-dev zstd cron
+apt install -y curl wget
 
 # 下载并安装 Alist
 curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install
@@ -39,4 +30,7 @@ useradd -m wrt && echo 'wrt:wrt' | chpasswd
 usermod -aG sudo wrt
 
 # 启动 SSH 守护进程
-/usr/sbin/sshd -D 
+/usr/sbin/sshd -D &
+
+# 输出文件执行完毕的提示
+echo "文件执行完毕"

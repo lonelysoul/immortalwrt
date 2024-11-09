cfbbf51d94b9c6eaba05a226659c9aea7eec3e05
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-09 23:07:08 +0800

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -17,7 +17,17 @@ mkdir -p /run/sshd
 echo 'root:root' | chpasswd
 
 # 安装开发和编译相关的工具
-apt install -y curl wget
+apt install -y  ack antlr3 asciidoc autoconf automake autopoint binutils bison \
+build-essential bzip2 ccache clang cmake cpio curl device-tree-compiler ecj \
+fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf \
+haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev \
+libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev \
+libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs \
+msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip \
+python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons \
+squashfs-tools subversion swig texinfo uglifyjs unzip vim wget xmlto \
+xxd zlib1g-dev zstd cron
+
 
 # 下载并安装 Alist
 curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install

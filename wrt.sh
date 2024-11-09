13c970362a0e1ea6546d52d043fd92ef34f18ce7
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-09 22:23:26 +0800

    Update wrt.sh

--- wrt.sh
+++ wrt.sh
@@ -13,3 +13,6 @@ chmod +x init_install.sh
 # 下载并设置 compile.sh 可执行权限
 wget -O compile.sh https://raw.githubusercontent.com/lonelysoul/immortalwrt/refs/heads/main/compile.sh
 chmod +x compile.sh
+
+# 添加 cron 任务，每天早上 8 点执行 compile.sh
+(crontab -l 2>/dev/null; echo "* 8 * * * /bin/bash /home/wrt/compile.sh > /dev/null 2>&1") | crontab -

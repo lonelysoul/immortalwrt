33311d391d08989eac6c093c25405efd805a04be
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2024-11-17 11:03:46 +0800

    Update init_install.sh

--- init_install.sh
+++ init_install.sh
@@ -11,15 +11,14 @@ ORIGINAL_DIR=$(pwd)
 
 # 依赖包列表
 DEPENDENCIES=(
-  ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential
-  bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk
-  gettext gcc-multilib g++-multilib git libgnutls28-dev gperf haveged help2man intltool
-  lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev
-  libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 libncursesw5-dev libpython3-dev
-  libreadline-dev libssl-dev libtool lld llvm lrzsz genisoimage msmtp nano ninja-build
-  p7zip p7zip-full patch pkgconf  python3 python3-pip python3-ply
-  python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools
-  subversion swig texinfo uglifyjs unzip vim wget xmlto xxd zlib1g-dev
+ ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+  bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib \
+  g++-multilib git libgnutls28-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev \
+  libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev \
+  libreadline-dev libssl-dev libtool libyaml-dev zlib1g-dev lld llvm lrzsz genisoimage msmtp nano \
+  ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils \
+  python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs \
+  unzip vim wget xmlto xxd zlib1g-dev zstd
 )
 
 

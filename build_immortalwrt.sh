cea9c4c4b8a95868239fad4ce0050d4dffbdfdae
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-05-19 07:41:40 +0800

    更新依赖

--- build_immortalwrt.sh
+++ build_immortalwrt.sh
@@ -11,14 +11,7 @@ REPO_PATH="${PWD}/immortalwrt"
 CONFIG_URL="https://raw.githubusercontent.com/lonelysoul/immortalwrt/main/.config"
 
 APT_PKGS=(
-ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential
-bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib
-g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev
-libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev
-libreadline-dev libssl-dev libtool libyaml-dev libz-dev lld llvm lrzsz mkisofs msmtp nano
-ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils
-python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs
-upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd
+build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev python3-setuptools rsync swig unzip zlib1g-dev file wget ccache
 )
 
 ########################################

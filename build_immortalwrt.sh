d4da38eb6125ede4b2cbed860c28ad3b3eeff64d
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-06-09 23:16:15 +0800

    add luci-app-mosdns

--- build_immortalwrt.sh
+++ build_immortalwrt.sh
@@ -97,6 +97,10 @@ update_feeds() {
     log "更新 feeds..."
     ./scripts/feeds update -a
     ./scripts/feeds install -a
+    find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+    find ./ | grep Makefile | grep mosdns | xargs rm -f
+    git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+    git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 }
 
 ########################################

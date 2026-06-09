4e92773c3def8bda266a3eab14bdb97e8f7b92d6
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-06-09 23:34:07 +0800

    fix mosdns

--- build_immortalwrt.sh
+++ build_immortalwrt.sh
@@ -99,6 +99,8 @@ update_feeds() {
     ./scripts/feeds install -a
     find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
     find ./ | grep Makefile | grep mosdns | xargs rm -f
+    rm -rf package/mosdns
+    rm -rf package/v2ray-geodata
     git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
     git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 }

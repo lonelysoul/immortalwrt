b50dccd4e72db7f43c19255c4267c2cbf08c05d3
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-08-06 11:23:23 +0800

    Add new feed sources to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,4 @@
-
+echo 'src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git' >>feeds.conf.default
+echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git' >>feeds.conf.default
+echo 'src-git kdae https://github.com/QiuSimons/luci-app-dae' >>feeds.conf.default
+echo 'src-git ddnsgo  https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default

5a014228afc62f0450c1a94919e5a305173034aa
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-08-08 17:57:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,2 +1,3 @@
-rm -rf feeds/packages/lang/rust
-git clone https://github.com/sbwml/packages_lang_rust feeds/packages/lang/rust
+# rm -rf feeds/packages/lang/rust
+# git clone https://github.com/sbwml/packages_lang_rust feeds/packages/lang/rust
+sed -i 's/--set=llvm\.download-ci-llvm=false/--set=llvm.download-ci-llvm=true/' feeds/packages/lang/rust/Makefile

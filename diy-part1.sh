cb038e4dda8e9a76d67b24530ea72d90f4f902ed
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2026-08-08 11:37:00 +0800

    add honk to feed,备用

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,3 +2,4 @@ echo 'src-git kixdns https://github.com/JohnsonRan/luci-app-kixdns.git' >>feeds.
 # echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git' >>feeds.conf.default
 echo 'src-git kdae https://github.com/QiuSimons/luci-app-dae.git' >>feeds.conf.default
 echo 'src-git ddnsgo  https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
+# echo 'src-git openwrthonk  https://github.com/breeze303/openwrt-honk.git' >>feeds.conf.default
